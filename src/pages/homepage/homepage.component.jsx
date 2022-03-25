97c70069c6c334631debdaae2a7a9181e72c9be7
Paulina <pn.1990@wp.pl>
2022-03-25 21:55:15 +0000

    button added

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -6,7 +6,9 @@ export default function Homepage(props) {
 const {allGames} = props
  return (
   <main className="homepage">
-   <Link to='/search'>Search all games </Link>
+   <Link to='/search'>
+    <img src="https://db3pap005files.storage.live.com/y4mdnq1WAFqcxjKvK22eT1vzPyPpgn1vHUgyFEPhlormx5Kf8CvyGgm9HqeQhQSvc8LmcDQ5Wn3cpJgBk5JHrBwGcjdw2dJUTO44EWWUur7UpjdClu8dc6ciqS-YFXRtiecZJLxUfaE2UfDT7Or0hooiYbXxqhSpcUj3ztMuF0NSiEv7OdCVZL6HLeE58s4x8Tj?width=256&height=49&cropmode=none" alt="search button" />
+   </Link>
    {allGames.map(game => <Game key={game._id} game={game} isLinkNeeded={true} />)}
    
   </main>
